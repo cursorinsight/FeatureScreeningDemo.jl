04b9b7ab9351c83b4477cd7617ad62f021bb7012
Oliver Mate Torteli <oliver@cursorinsight.com>
2021-08-16 20:44:08 +0200

    feat: Implement visualization for `Measurement`s
    - plot could contain an "upper hull"

--- src/Visualization.jl
+++ src/Visualization.jl
@@ -7,12 +7,19 @@
 module Visualization
 
 ###=============================================================================
+### Exports
+###=============================================================================
+
+export plot
+
+###=============================================================================
 ### Imports
 ###=============================================================================
 
-import PlotlyJS: plot
+import PlotlyJS: plot, scatter
 using FeatureScreening.Types: FeatureSet, labels, features
-using PlotlyJS: scatter
+using FeatureScreeningDemo.Utilities: upper_hull
+using FeatureScreeningDemo.Benchmarking: Measurement, metric, config
 
 ###=============================================================================
 ### Implementation
@@ -33,6 +40,83 @@ function plot(feature_set::FeatureSet{L}) where {L}
                      scatter(x = x, y = y, name = label, mode = "markers")
                  end
                  for (label, idxs) in sort!(collect(idxs); by = first)])
+end
+
+# TODO refactor
+function scatter(measurements::Array{Measurement};
+                 x::Symbol = keys(metrics(measurements[1]))[1], # TODO remove
+                 y::Symbol = keys(metrics(measurements[1]))[2], # TODO remove
+                 kwargs...)
+    x::Vector{<: Real} = metric.(measurements, x) |> vec
+    y::Vector{<: Real} = metric.(measurements, y) |> vec
+    text::Vector{String} = scatter_text(measurements) |> vec
+    return scatter(; x, y, text, kwargs...)
+end
+
+# TODO refactor
+function plot(measurements::Array{Measurement};
+              group_by::Symbol,
+              x::Symbol,
+              y::Symbol,
+              format::String = "",
+              hull::Bool = true,
+              kwargs...
+             )
+    line_configs = unique(complement.(config.(measurements), group_by))
+    common_config_pairs = foldl(intersect, pairs.(line_configs))
+    netto_line_configs = map(line_configs) do config
+        return NamedTuple(filter(c -> !(c[1] in keys(common_config_pairs)),
+                                 pairs(config)))
+    end
+    data_series = [select_data(measurements, lc) for lc in line_configs]
+    lines = [scatter(data_series[i];
+                     x,
+                     y,
+                     name = format_name(format, netto_line_configs[i]),
+                     kwargs...)
+             for i in eachindex(netto_line_configs)]
+    if hull
+        points = [[mm.metrics[x], mm.metrics[y]] for mm in vec(measurements)]
+        points = upper_hull(points)
+        x = first.(points)
+        y = last.(points)
+        push!(lines, scatter(; x, y, name = "Envelope", line_width = 5))
+    end
+    return plot(lines)
+end
+
+# TODO remove/refactor
+function scatter_text(measurements::Array{Measurement, N}
+                     )::Array{String, N} where {N}
+    return scatter_text.(measurements)
+end
+
+# TODO remove/refactor
+function scatter_text(measurement::Measurement)::String
+    return join(["$k:$v" for (k, v) in pairs(config(measurement))], '\n')
+end
+
+# TODO remove/refactor
+function complement(nt::NamedTuple, exempt_key::Symbol)
+    return NamedTuple(filter(nt -> nt[1] != exempt_key, pairs(nt)))
+end
+
+function format_name(fmt::String, config::NamedTuple)
+# TODO remove/refactor
+    if (fmt == "")
+        return join(["$(k):$(config[k])" for k in keys(config)], "_")
+    end
+    regex = Regex(join((k -> "{$(k)}").(keys(config)), "|"))
+    return replace(fmt, regex => s -> config[Symbol(s[2:end-1])])
+end
+
+# TODO remove/refactor
+function select_data(measurements::Array{Measurement},
+                     criteria::NamedTuple
+                    )::Array{Measurement}
+    return filter(measurements) do m
+        return all(m.config[k] == criteria[k] for k in keys(criteria))
+    end
 end
 
 end # module
